7d62c56e6c09badca6cb67df94e6221335ab7e67
Enrico Zini <enrico@enricozini.org>
2009-11-21 16:20:21 +0000

    Perform screen blanking in the dynamic plugin

--- hooks/omhacks.c
+++ hooks/omhacks.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include <omhacks/sysfs.h>
 #include <omhacks/led.h>
 #include <string.h>
+#include <stdlib.h>
 
 typedef int hook_function(const char* name, const char* param);
 extern void hooks_add_function(const char* name, hook_function func);
 
 static struct om_led status_led;
 static struct om_led status_led_saved;
+static int screen_brightness_saved;
 
 static int hook_status_led(const char* name, const char* param)
 {
@@ -39,6 +42,27 @@ static int hook_status_led(const char* name, const char* param)
 	return 0;
 }
 
+static int hook_screen(const char* name, const char* param)
+{
+	if (strcmp(param, "suspend") == 0)
+	{
+		// Save current backlight brightness and turn it off
+		const char* val = om_sysfs_swap("brightness", "0");
+		if (val == NULL)
+			screen_brightness_saved = 255;
+		else
+			screen_brightness_saved = atoi(val);
+	}
+	else
+	{
+		// Restore saved backlight brightness
+		char val[20];
+		snprintf(val, 20, "%d", screen_brightness_saved);
+		om_sysfs_set("brightness", val);
+	}
+	return 0;
+}
+
 void init()
 {
 	if (om_led_init(&status_led, "gta02-power:blue") == 0
@@ -47,4 +71,7 @@ void init()
 		hooks_add_function("00-statusled", hook_status_led);
 		hooks_add_function("99-statusled", hook_status_led);
 	}
+
+	hooks_add_function("74-screen", hook_screen);
+	hooks_add_function("74-screen", hook_screen);
 }
