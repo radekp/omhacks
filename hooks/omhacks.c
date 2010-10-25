bd3a70a279a9a2976ffed9476ee3221bd5940925
Enrico Zini <enrico@enricozini.org>
2010-10-25 22:30:50 +0200

    Another unchecked error code

--- hooks/omhacks.c
+++ hooks/omhacks.c
@@ -21,15 +21,21 @@ static int hook_00_status_led(int argc, const char* argv[])
     if (strcmp(argv[1], "suspend") == 0)
     {
         // Save blue led state and turn it on
-        om_led_get(&status_led_saved);
-        status_led.brightness = 255;
-        om_led_set(&status_led);
+        if (om_led_get(&status_led_saved) < 0)
+        {
+            perror("reading blue led status");
+            status_led.brightness = -1;
+        } else {
+            status_led.brightness = 255;
+            om_led_set(&status_led);
+        }
         return 0;
     }
     else if (strcmp(argv[1], "resume") == 0)
     {
         // Restore blue led state
-        om_led_set(&status_led_saved);
+        if (status_led.brightness != -1)
+            om_led_set(&status_led_saved);
         return 0;
     }
 
@@ -43,15 +49,21 @@ static int hook_99_status_led(int argc, const char* argv[])
     if (strcmp(argv[1], "suspend") == 0)
     {
         // Turn off blue state before suspend
-        status_led.brightness = 0;
-        om_led_set(&status_led);
+        if (status_led.brightness != -1)
+        {
+            status_led.brightness = 0;
+            om_led_set(&status_led);
+        }
         return 0;
     }
     else if (strcmp(argv[1], "resume") == 0)
     {
         // Turn on blue led after resume
-        status_led.brightness = 255;
-        om_led_set(&status_led);
+        if (status_led.brightness != -1)
+        {
+            status_led.brightness = 255;
+            om_led_set(&status_led);
+        }
         return 0;
     }
 
@@ -115,6 +127,7 @@ static int hook_cancel_on_usb_disconnect(int argc, const char* argv[])
     }
     return 254;
 }
+
 void init()
 {
     if (om_led_init(&status_led, "gta02-power:blue") == 0
